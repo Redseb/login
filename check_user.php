c0073ecfac65a4a40f6c4f0c4518aa8eb5838c51
Miko Zyzanski <dwarfslikepie@gmail.com>
2017-02-06 13:10:32 +0100

    Create check_user.php

---
+++ check_user.php
@@ -0,0 +1,23 @@
+<?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$validUser = "username";
+$validPassword = "1234";
+
+echo "You have passed username: $username and a password of : $password";
+
+if ($username == $validUser && $password == $validPassword){
+    echo "Welcome to the club pal";
+} else {
+    echo "Wrong Password or Username";
+}
+
+
+
+/**
+ * Created by PhpStorm.
+ * User: 18zyzanski_m
+ * Date: 03/02/17
+ * Time: 09:12
+ */
