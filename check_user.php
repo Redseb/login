27652936ad669e7a0b0f14014a2b5dff4d1fc331
Redseb <dwarfslikepie@gmail.com>
2017-02-06 13:25:12 +0100

    Added an exclemation mark (x2)

--- check_user.php
+++ check_user.php
@@ -8,7 +8,7 @@ $validPassword = "1234";
 echo "You have passed username: $username and a password of : $password";
 
 if ($username == $validUser && $password == $validPassword){
-    echo "Welcome to the club pal!";
+    echo "Welcome to the club pal!!";
 } else {
     echo "Wrong Password or Username";
 }
